153c13ab64061f32e500ad1b9f49b274058cf682
perry.dahl.christensen@gmail.com <perry.dahl.christensen@gmail.com@38b8aa26-8b72-93d6-1474-526a1923a794>
2011-04-11 15:36:30 +0000

    fddfsd

---
+++ trunk/mdmi_midi/src/mdmi_midi.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Perry D Christensen"
+__date__ ="$11-04-2011 17:27:16$"
+
+if __name__ == "__main__":
+    print "Hello World"
